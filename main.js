0681637fe61ab23e6119789665726293e7e19d65
Jason Hines <jason@greenhell.com>
2021-08-31 18:10:44 -0400

    cleanup

--- main.js
+++ main.js
@@ -19,30 +19,28 @@ Client.once('ready', () => {
 
     setInterval(() => {
         Config.SERVERS.forEach((Server) => {
-            getServerStatus(Server.CFTOOLS_HOSTNAME, Server.CFTOOLS_PORT).then((details) => {
-
+            pollGameServerDetails(Server.CFTOOLS_HOSTNAME, Server.CFTOOLS_PORT).then((details) => {
                 let player_stats = details.status.players;
                 console.log(`Polled server ${Server.NAME} (${Server.CFTOOLS_HOSTNAME}:${Server.CFTOOLS_PORT})...`);
                 console.log(player_stats);
 
-                let channel = Client.channels.cache.get(Server.CHANNEL_ID)
-                let new_channel_name = `${Server.NAME}: ${ player_stats.online }/${ player_stats.slots }`
+                Client.channels.fetch(Server.CHANNEL_ID).then((channel) => {
+                    let new_channel_name = `${Server.NAME}: ${player_stats.online}/${player_stats.slots}`
 
-                if (channel.name == new_channel_name) return;
+                    // channel name is unchanged
+                    if (channel.name == new_channel_name) return;
+    
+                    channel.setName(new_channel_name).catch(console.error);
+                }).catch(console.error);
 
-                channel.setName(new_channel_name)
-                    .then(newChannel => console.log(`Setting channel name: ${newChannel.name}`))
-                    .catch(console.error);
-            }).catch(function (error) {
-                console.log(error);
-            });
+            }).catch(console.error);
         });
     }, Config.POLLING_INTERVAL * 1000);
 });
 
 Client.login(Config.BOT_TOKEN);
 
-async function getServerStatus(hostname, port) {
+async function pollGameServerDetails(hostname, port) {
     let cftools_client = new CFToolsClientBuilder().build();
     let details = cftools_client.getGameServerDetails({
         game: Game.DayZ,
