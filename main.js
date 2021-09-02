0e543c55ea2f21126edbb461c9feb3bc281019e0
Jason Hines <jason@greenhell.com>
2021-09-02 15:54:23 -0400

    support offline

--- main.js
+++ main.js
@@ -32,14 +32,20 @@ Client.once('ready', () => {
             pollGameServerDetails(CFClient, Server).then((details) => {
                 let player_stats = details.status.players;
                 console.log(`Polled server ${Server.NAME} (${Server.CFTOOLS_HOSTNAME}:${Server.CFTOOLS_PORT})...`);
-                console.log(player_stats);
 
                 Client.channels.fetch(Server.CHANNEL_ID).then((channel) => {
-                    let new_channel_name = `${Server.NAME}: ${player_stats.online}/${player_stats.slots}`
+                    let new_channel_name;
+                    if (details.online) {
+                        // todo player_stats.queue
+                        new_channel_name = `${Server.NAME}: ${player_stats.online}/${player_stats.slots}`
+                    } else {
+                        new_channel_name = `${Server.NAME}: offline`
+                    }
 
                     // channel name is unchanged
                     if (channel.name == new_channel_name) return;
     
+                    console.log(`Setting channel name: ${new_channel_name}`);
                     channel.setName(new_channel_name).catch(console.error);
                 }).catch(console.error);
 
